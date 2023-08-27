277e30a9140e448295cf2ad47bd04f32662125ab
123 <zlata18_00@mail.ru>
2023-08-27 17:07:26 +0300

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        task1();}
+
+public static void task1(){
+        System.out.println("Задача 1");
+        int balance = 0;
+         int month = 0;
+         while (balance < 2_459_000){
+             balance+=15000;
+             ++month;
+         }
+         System.out.println("Месяц"+month+",сумма накоплений равна"+ balance+"рублей");
+
+}}
